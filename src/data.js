7da98f009fa589a94a95832cd13dbf997bc29dfd
Ana Marlen Aguilar Lòpez <ag.lo.am26@gmail.com>
2021-07-14 14:07:33 -0500

    Primer funcion trae todos los personajes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
+
+import data from './data/rickandmorty/rickandmorty.js';
+const information = {
+    showCharacters() {
+        const characters = data.results.map(function(character) {
+            return character;
+        });
+        console.log(characters);
+    }
+}
+
+export default information
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
+import information from './data.js';
+document.getElementById("button").addEventListener("click", function() {
+    information.showCharacters();
+});
+
+
+
+
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
