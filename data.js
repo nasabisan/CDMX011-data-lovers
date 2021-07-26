896b812ae7608053c7fef18f4d5b5115b933bb59
Karli <nasabisan@gmail.com>
2021-07-25 22:23:02 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,170 @@
+const information = {
+
+    sortData(data, select) {
+
+        let orderArray;
+        switch (select) {
+
+            case 1:
+                // Name/Ascendente
+
+                orderArray = data.sort(function(a, b) {
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                    if (a.name > b.name) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                })
+                break;
+
+            case 2:
+                //name/descendente
+
+                orderArray = data.sort(function(a, b) {
+                    if (b.name < a.name) {
+                        return -1
+                    }
+                    if (b.name > a.name) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                })
+                break;
+
+            case 3:
+                //id/ascedente
+
+
+                orderArray = data.sort(function(a, b) {
+                    return (a.id - b.id)
+                })
+                break;
+
+            case 4:
+                //id/descendente
+
+                orderArray = data.sort(function(a, b) {
+                    return (b.id - a.id)
+                })
+                break;
+            default:
+        }
+
+        return orderArray
+
+
+
+
+    },
+    filterData(data, select) {
+
+        let filterArray;
+        switch (select) {
+
+
+            case 1:
+                // Alives
+
+                filterArray = data.filter(function(data) {
+                    return data.status === 'Alive';
+                })
+
+                break;
+
+            case 2:
+                //Deads
+
+                filterArray = data.filter(function(data) {
+                    return data.status === 'Dead';
+                })
+
+
+                break;
+
+            case 3:
+                //Humans
+
+
+                filterArray = data.filter(function(data) {
+                    return data.species === 'Human';
+                })
+                break;
+
+            case 4:
+                //Aliens
+
+
+                filterArray = data.filter(function(data) {
+                    return data.species === 'Alien';
+                })
+                break;
+
+            case 5:
+                //Humanoid
+
+
+                filterArray = data.filter(function(data) {
+                    return data.species === 'Humanoid'
+                })
+                break;
+
+            case 6:
+                //Poopybuttholes
+
+
+                filterArray = data.filter(function(data) {
+                    return data.species === 'Poopybutthole'
+                })
+                break;
+            case 7:
+                //Cronenbergs
+
+
+                filterArray = data.filter(function(data) {
+                    return data.species === 'Cronenberg'
+                })
+                break;
+
+            case 8:
+                //Males
+
+
+                filterArray = data.filter(function(data) {
+                    return data.gender === 'Male'
+                })
+                break;
+            case 9:
+                //Females
+
+
+                filterArray = data.filter(function(data) {
+                    return data.gender === 'Female'
+                })
+                break;
+            default:
+                return data
+        }
+        return filterArray
+
+
+
+
+    },
+
+    findData(input, data) {
+
+        let search
+
+        search = data.filter(function(data) {
+            return data.name === input
+        })
+        return search
+    }
+}
+
+
+export default information
---
+++ main.js
@@ -0,0 +1,116 @@
+import data from './data/rickandmorty/rickandmorty.js';
+import information from './data.js';
+
+
+
+let charactersCounter = 0;
+let i;
+const character = data.results
+
+
+
+
+//const next = document.getElementById('next');
+//const prev = document.getElementById('prev');
+const sort = document.getElementById('sort');
+const filter = document.getElementById('filter');
+const search = document.getElementById('input')
+let searchValue = document.getElementById('input').value;
+let filterValue = document.getElementById('filter').value;
+let sortValue = document.getElementById('sort').value;
+
+///const newArray = Array.from(information.filterData(character, parseInt(filterValue)))
+//console.log(newArray)
+
+
+window.onload = function() {
+    showInScreen(information.sortData(character, 3))
+    changeSearch(searchValue)
+}
+
+/*next.addEventListener('click', forward())
+
+function forward() {
+    charactersCounter += 9
+    showInScreen(information.filterData(information.sortData(character, parseInt(sortValue)), parseInt(filterValue)))
+
+}
+
+prev.addEventListener('click', backward())
+
+function backward() {
+    charactersCounter -= 9
+    showInScreen(information.filterData(information.sortData(character, parseInt(sortValue)), parseInt(filterValue)))
+
+}*/
+
+sort.addEventListener('click', function() {
+    filterValue = document.getElementById('filter').value;
+    sortValue = document.getElementById('sort').value;
+    return showInScreen(information.filterData(information.sortData(character, parseInt(sortValue)), parseInt(filterValue)))
+})
+
+filter.addEventListener('click', function() {
+    filterValue = document.getElementById('filter').value;
+    return showInScreen(information.filterData(character, parseInt(filterValue)))
+})
+
+search.addEventListener('keyup', function() {
+    searchValue = document.getElementById('input').value;
+    return showInScreen((information.findData(changeSearch(), character)))
+})
+
+function showInScreen(arreglo) {
+
+    let newHTML = "";
+    for (i = 0; i < arreglo.length; i++) {
+
+        newHTML += `  <div class='grid'>
+                        <section class='identifier'>
+                        <p>${arreglo[i+charactersCounter].name}</p>
+                        <img class='image' src="${arreglo[i+charactersCounter].image}">
+                        </section>
+                        <section class='properties'>
+                        <p><span class='keys'>ID: </span> ${arreglo[i+charactersCounter].id}</p>
+                        <p><span class='keys'>Gender: </span> ${arreglo[i+charactersCounter].gender}</p>
+                        <p><span class='keys'>Species: </span>${arreglo[i+charactersCounter].species}</p>
+                        <p><span class='keys'>Status: </span> ${arreglo[i+charactersCounter].status} <span data-status="${arreglo[i].status}" class='dot'>-</span></p>
+                        <p><span class='keys'>Origin: </span>${arreglo[i+charactersCounter].origin.name}</p>
+                        </section>
+                    </div>`
+    }
+    return document.getElementById("characters").innerHTML = newHTML, changeColor()
+}
+
+
+
+
+function changeColor() {
+
+    const pointElements = document.querySelectorAll('.dot')
+    for (i = 0; i < pointElements.length; i++) {
+
+        if (pointElements[i].dataset.status == "Alive") {
+
+            pointElements[i].style.background = '#5EBD3C';
+            pointElements[i].style.color = '#5EBD3C';
+
+        } else if (pointElements[i].dataset.status == "unknown") {
+            pointElements[i].style.background = '#E7E866';
+            pointElements[i].style.color = '#E7E866';
+        }
+    }
+
+}
+
+function changeSearch() {
+    let fullname = document.getElementById('input').value
+    let initial1 = fullname.slice(0, 1);
+    let positioninitial2 = fullname.indexOf(" ") + 1;
+    let initial2 = fullname.slice(positioninitial2, positioninitial2 + 1);
+    let str = initial1.toUpperCase() + (fullname.slice(1, positioninitial2 - 1)).toLowerCase() + " " + initial2.toUpperCase() + (fullname.slice(positioninitial2 + 1, fullname.length)).toLowerCase()
+        // console.log(str);
+    return str
+
+
+}
